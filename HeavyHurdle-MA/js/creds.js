20ba929a3253bfdce9fb2b98473fac782bc33b44
Karan Shah <karanshah0206@outlook.com>
2020-07-21 15:27:35 +0530

    Gets The User Credentials

---
+++ HeavyHurdle-MA/js/creds.js
@@ -0,0 +1,24 @@
+// Getting Credentials
+const room = getCreds('room');
+const user = sessionStorage.getItem('HHUser');
+
+// Uses GET For Javascript
+function getCreds(key='') {
+    var url = location.href;
+    var query = decodeURIComponent(url).split('#', 2)[0].split('?', 2)[1];
+    if(query){
+        let splittedQStrings = query.split('&');
+        if(splittedQStrings.length){
+            let queryStringObj = {};
+            splittedQStrings.forEach(function(keyValuePair){
+                let keyValue = keyValuePair.split('=', 2);
+                if(keyValue.length){
+                    queryStringObj[keyValue[0]] = keyValue[1];
+                }
+            });            
+            return key ? (queryStringObj[key] ? queryStringObj[key] : null) : queryStringObj;
+        }
+        return null;
+    }
+    return null;
+}
