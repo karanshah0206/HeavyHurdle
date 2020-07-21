db6c5342ba202582b0670a1168bd46d8a01e9446
Karan Shah <karanshah0206@outlook.com>
2020-07-21 09:35:01 +0530

    On/Off Control List

---
+++ HeavyHurdle-MA/js/ui.js
@@ -0,0 +1,7 @@
+// Control List Toggle
+document.getElementById('allControl').addEventListener('click', () => {
+    document.getElementById('listControls').classList.toggle('hidden');
+});
+document.getElementById('listControls').addEventListener('click', () => {
+    document.getElementById('listControls').classList.toggle('hidden');
+});
