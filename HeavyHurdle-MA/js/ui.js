a11a1d6da1d41f8fa05f83a644745d38b11e9db0
Karan Shah <karanshah0206@outlook.com>
2020-07-21 11:37:34 +0530

    Added Videos + Present/Discuss Layout Responsive

--- HeavyHurdle-MA/js/ui.js
+++ HeavyHurdle-MA/js/ui.js
@@ -3,5 +3,21 @@ document.getElementById('allControl').addEventListener('click', () => {
     document.getElementById('listControls').classList.toggle('hidden');
 });
 document.getElementById('listControls').addEventListener('click', () => {
-    document.getElementById('listControls').classList.toggle('hidden');
+    document.getElementById('listControls').classList.add('hidden');
+});
+
+// Change Mode
+document.getElementById('changeMode').addEventListener('click', () => {
+    if (document.getElementById('remoteVid').classList.contains('presentation')) {
+        document.getElementById('remoteVid').classList.remove('presentation');
+        document.getElementById('present').classList.add('hidden');
+        document.getElementById('remoteVid').classList.add('discussion');
+        document.getElementById('changeMode').innerHTML = '<i class="fas fa-object-group"></i> Enter Presentation Mode';
+    }
+    else {
+        document.getElementById('remoteVid').classList.remove('discussion');
+        document.getElementById('remoteVid').classList.add('presentation');
+        document.getElementById('present').classList.remove('hidden');
+        document.getElementById('changeMode').innerHTML = '<i class="fas fa-object-group"></i> Enter Discussion Mode';
+    }
 });
