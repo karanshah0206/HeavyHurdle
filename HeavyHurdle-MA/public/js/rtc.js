9267430ad5ee4b061fb3f450adba4b45556a923f
Karan Shah <karanshah0206@outlook.com>
2020-09-17 05:22:56 +0530

    Agora Basic Video Call Implemented

---
+++ HeavyHurdle-MA/public/js/rtc.js
@@ -0,0 +1,50 @@
+let handleFail = function (err) { console.log("Error: ", err); }
+
+let remoteContainer = document.getElementById("remoteVid");
+let client  = AgoraRTC.createClient({ mode: "rtc", codec: "vp8" });
+client.init("c0041179099d492fa2dafcc82ec735c0");
+
+function addVideoStream (elementId) {
+    let streamDiv = document.createElement("div");
+    streamDiv.id = elementId;
+    streamDiv.style.transform = "rotateY(180deg)"; // Mirroring video
+    remoteContainer.appendChild(streamDiv);
+}
+
+function removeVideoStream (elementId) {
+    let remDiv = document.getElementById(elementId);
+    if (remDiv) remDiv.parentNode.removeChild(remDiv);
+}
+
+client.join(null, room, null, (uid) => {
+    let localStream = AgoraRTC.createStream({ video: true, audio: true });
+    localStream.init(() => {
+        //localStream.play("remoteVid");
+        client.publish(localStream, handleFail);
+    }, handleFail);
+}, handleFail);
+
+client.on("stream-added", (evt) => {
+    client.subscribe(evt.stream, handleFail);
+}, handleFail);
+
+client.on("stream-subscribed", (evt) => {
+    let stream = evt.stream;
+    let streamId = String(stream.getId());
+    addVideoStream(streamId);
+    stream.play(streamId);
+}, handleFail);
+
+client.on("stream-removed", (evt) => {
+    let stream = evt.stream;
+    let streamId = String(stream.getId());
+    stream.close();
+    removeVideoStream(streamId);
+}, handleFail);
+
+client.on("peer-leave", (evt) => {
+    let stream = evt.stream;
+    let streamId = String(stream.getId());
+    stream.close();
+    removeVideoStream(streamId);
+}, handleFail);
